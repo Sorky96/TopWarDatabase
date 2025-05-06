51e429334a70d5f634732913b21674090e2516d2
Sorky96 <42413696+Sorky96@users.noreply.github.com>
2025-05-06 21:52:23 +0200

    Added tests

---
+++ Tests/ItemController/ItemControllerTests.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using TopWarModels;
+
+namespace TopWarDatabase.Tests
+{
+    public class ItemsControllerTests
+    {
+        private DbContextOptions<GameItemsContext> CreateNewContextOptions()
+        {
+            return new DbContextOptionsBuilder<GameItemsContext>()
+                .UseInMemoryDatabase(databaseName: System.Guid.NewGuid().ToString())
+                .Options;
+        }
+
+        [Fact]
+        public async Task GetItems_ReturnsAllItems()
+        {
+            var options = CreateNewContextOptions();
+            using var context = new GameItemsContext(options);
+            context.Items.Add(new Item
+            {
+                Name = "Laser Gun",
+                ImageUrl = "url",
+                Attributes = new List<ItemAttribute>()
+            });
+            await context.SaveChangesAsync();
+
+            var controller = new ItemsController(context);
+            var result = await controller.GetItems();
+
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            var items = Assert.IsAssignableFrom<IEnumerable<Item>>(okResult.Value);
+            Assert.Single(items);
+        }
+
+        [Fact]
+        public async Task GetItem_ReturnsCorrectItem()
+        {
+            var options = CreateNewContextOptions();
+            using var context = new GameItemsContext(options);
+            var item = new Item
+            {
+                Name = "Tank",
+                ImageUrl = "url",
+                Attributes = new List<ItemAttribute>()
+            };
+            context.Items.Add(item);
+            await context.SaveChangesAsync();
+
+            var controller = new ItemsController(context);
+            var result = await controller.GetItem(item.Id);
+
+            var returnedItem = Assert.IsType<Item>(result.Value);
+            Assert.Equal("Tank", returnedItem.Name);
+        }
+
+        [Fact]
+        public async Task CreateItem_AddsNewItem()
+        {
+            var options = CreateNewContextOptions();
+            using var context = new GameItemsContext(options);
+            var controller = new ItemsController(context);
+
+            var newItem = new Item
+            {
+                Name = "Drone",
+                ImageUrl = "url",
+                Attributes = new List<ItemAttribute>()
+            };
+
+            var result = await controller.CreateItem(newItem);
+            var createdResult = Assert.IsType<CreatedAtActionResult>(result.Result);
+            var item = Assert.IsType<Item>(createdResult.Value);
+            Assert.Equal("Drone", item.Name);
+        }
+
+        [Fact]
+        public async Task DeleteItem_RemovesItem()
+        {
+            var options = CreateNewContextOptions();
+            using var context = new GameItemsContext(options);
+            var item = new Item
+            {
+                Name = "Mech",
+                ImageUrl = "url",
+                Attributes = new List<ItemAttribute>()
+            };
+            context.Items.Add(item);
+            await context.SaveChangesAsync();
+
+            var controller = new ItemsController(context);
+            var result = await controller.DeleteItem(item.Id);
+
+            Assert.IsType<NoContentResult>(result);
+            Assert.False(context.Items.Any(i => i.Id == item.Id));
+        }
+    }
+}
---
+++ Tests/ModelTests/ItemTests.cs
@@ -0,0 +1,46 @@
+﻿using TopWarModels;
+
+namespace TopWarDatabase.Tests
+{
+    public class ItemTests
+    {
+        [Fact]
+        public void Item_WithAttributes_ShouldContainExpectedValues()
+        {
+            var attributes = new List<ItemAttribute>
+            {
+                new ItemAttribute {
+                    AttributeType = "Attack",
+                    AttributeValue = "10",
+                    Item = new Item { Id = 99, Name = "Dummy", ImageUrl = "url", Attributes = new List<ItemAttribute>() }
+                },
+                new ItemAttribute {
+                    AttributeType = "Defense",
+                    AttributeValue = "5",
+                    Item = new Item { Id = 99, Name = "Dummy", ImageUrl = "url", Attributes = new List<ItemAttribute>() }
+                }
+            };
+
+            var item = new Item
+            {
+                Id = 3,
+                Name = "Shield Generator",
+                ImageUrl = "https://example.com/shield.png",
+                Attributes = attributes
+            };
+
+            Assert.Collection(item.Attributes,
+                attr =>
+                {
+                    Assert.Equal("Attack", attr.AttributeType);
+                    Assert.Equal("10", attr.AttributeValue);
+                },
+                attr =>
+                {
+                    Assert.Equal("Defense", attr.AttributeType);
+                    Assert.Equal("5", attr.AttributeValue);
+                });
+        }
+
+    }
+}
